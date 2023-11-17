5fbfaf07b8ecc84a503847bf936bdc2cc6286d62
VamsiKrishnadulam <111721738+VamsiKrishnadulam@users.noreply.github.com>
2023-11-18 00:56:23 +0530

    demo

---
+++ src/main/java/com/app/demo/newPoject2/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.newPoject2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
